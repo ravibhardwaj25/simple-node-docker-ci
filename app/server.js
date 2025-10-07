701d8f43431d4e6c01e89960590007dbc20b4bfb
ravibhardwaj25 <125500967+ravibhardwaj25@users.noreply.github.com>
2025-10-07 23:49:45 +0530

    CHANGE IN MESSAGE (#2)

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Simple Node Docker CI App!');
+  res.send('Hello from Simple Node Docker CI App!!!');
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
